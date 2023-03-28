4745a8c3d6931da51b6e6d99dd3ec19fceeaa1cb
KyryloKalinichenko <kirillstomper@gmail.com>
2023-03-28 10:26:58 +0200

    Init commit

---
+++ src/main/java/com/example/eurder/domain/address/Address.java
@@ -0,0 +1,24 @@
+package com.example.eurder.domain.address;
+
+public class Address {
+    private String country;
+    private String city;
+    private String postCode;
+    private int homeNumber;
+
+    public String getCountry() {
+        return country;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public String getPostCode() {
+        return postCode;
+    }
+
+    public int getHomeNumber() {
+        return homeNumber;
+    }
+}
---
+++ src/main/java/com/example/eurder/domain/customer/Customer.java
@@ -0,0 +1,8 @@
+package com.example.eurder.domain.customer;
+
+public class Customer {
+    private String firstname;
+    private String lastname;
+    private String email;
+
+}
