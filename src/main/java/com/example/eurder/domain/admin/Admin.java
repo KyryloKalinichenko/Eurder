15acb9e2e930dc0489415137b70ff1be2dbf649a
KyryloKalinichenko <kirillstomper@gmail.com>
2023-03-28 15:11:33 +0200

    + Admin

---
+++ src/main/java/com/example/eurder/domain/admin/Admin.java
@@ -0,0 +1,9 @@
+package com.example.eurder.domain.admin;
+
+import com.example.eurder.domain.user.User;
+
+public class Admin extends User {
+    public Admin(String firstname, String lastname) {
+        super(firstname, lastname);
+    }
+}
